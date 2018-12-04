71d1d1662cb2cec3cdeb4bdeb982e16921cb0a17
Corentin <corentin.terente@gmail.com>
2018-12-04 14:16:56 +0100

    Initial Commit

---
+++ SuperQuizz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SuperQuizz
+//
+//  Created by formation2 on 04/12/2018.
+//  Copyright © 2018 formation2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
