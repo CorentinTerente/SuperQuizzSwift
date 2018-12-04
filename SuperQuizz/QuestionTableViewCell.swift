09fed8cdb2bf223acab29a18de049d677bde3d3b
Corentin <corentin.terente@gmail.com>
2018-12-04 15:16:08 +0100

    initialisation du projet SuperQuizz

---
+++ SuperQuizz/QuestionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  QuestionTableViewCell.swift
+//  SuperQuizz
+//
+//  Created by formation2 on 04/12/2018.
+//  Copyright © 2018 formation2. All rights reserved.
+//
+
+import UIKit
+
+class QuestionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
